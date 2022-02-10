44559dd602a7b52e2a32df064d89e6ad5efcb1e2
Italo Gonçales <italo.goncales@gmail.com>
2022-02-09 21:19:15 -0300

    Adding a limit to the hero on the board

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -11,40 +11,40 @@ const Hero = () => {
     y: 2,
   });
 
-  useEventListener("keydown", ({ key }) => {
+  const move = (x: number, y: number) => {
+    if (x < 1) x = 1;
+    if (y < 2) y = 2;
+    if (x > 18) x = 18;
+    if (y > 17) y = 17;
+
+    setPosition({
+      x,
+      y,
+    });
+  };
+
+  useEventListener("keydown", ({ key }: KeyboardEvent) => {
     console.log(key);
 
     switch (key.toUpperCase()) {
       case "ARROWLEFT":
       case "A": {
-        setPosition((position) => ({
-          x: position.x - 1,
-          y: position.y,
-        }));
+        move(position.x - 1, position.y);
         break;
       }
       case "ARROWRIGHT":
       case "D": {
-        setPosition((position) => ({
-          x: position.x + 1,
-          y: position.y,
-        }));
+        move(position.x + 1, position.y);
         break;
       }
       case "ARROWUP":
       case "W": {
-        setPosition((position) => ({
-          x: position.x,
-          y: position.y + 1,
-        }));
+        move(position.x, position.y + 1);
         break;
       }
       case "ARROWDOWN":
       case "S": {
-        setPosition((position) => ({
-          x: position.x,
-          y: position.y - 1,
-        }));
+        move(position.x, position.y - 1);
         break;
       }
     }
